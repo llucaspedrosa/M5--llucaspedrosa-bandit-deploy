97f750d0891fcf0387b9b9eb58316cc9b5ec8519
Lucas <lucaspedrosadesouza@gmail.com>
2023-02-09 18:00:20 -0300

    createDevelop

---
+++ Desktop/Curso Kenzie/Projetos para testes/Teste para Copybase/PokemonApi/src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HeaderComponent from "../components/HeaderComponent.vue";
+import ButtonComponent from "../components/ButtonComponent.vue";
+</script>
+
+<template>
+  <main>
+    <HeaderComponent />
+    <ButtonComponent msg="teste" />
+  </main>
+</template>
